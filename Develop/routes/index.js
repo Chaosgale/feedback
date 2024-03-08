a80f003d865735695787a5571611682a3c406a5d
Yan Ashe <yanashe@Yans-Mac-Studio.local>
2024-03-07 21:15:26 -0600

    fixed index routing

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -3,10 +3,10 @@ const router = require('express').Router();
 // Import our modular routers for /tips and /feedback
 const tipsRouter = require('./tips');
 const feedbackRouter = require('./feedback');
-// TODO: import your diagnostics route
 const diagnosticsrouter = require('./diagnostics');
+
 router.use('/tips', tipsRouter);
 router.use('/feedback', feedbackRouter);
-// TODO: Initialize diagnostics route
-router.use(diagnosticsrouter);
+router.use('./diagnostics', diagnosticsrouter);
+
 module.exports = router;
